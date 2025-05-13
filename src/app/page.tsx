7a7ae28e6d8b2fd9617e00f1c49a24ec09edf7dc
heyitsGaspar <israel.chay.32@gmail.com>
2025-05-13 15:03:55 -0600

    add motion and img2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,48 @@
-import Image from 'next/image';
-import { TextGenerateEffect } from '@/components/ui/text-generate-effect';
+'use client'
+import Image from 'next/image'
+import { TextGenerateEffect } from '@/components/ui/text-generate-effect'
+import { motion} from 'motion/react'
+const text = 'Bienvenidos a Los Primos'
+const text2 = 'Hamburguesas, hot dogs, esquites, y más...'
+const text3 = 'Explora nuestros productos '
 
-const text = 'Bienvenidos a Los Primos';
-const text2 = 'Explora nuestro contenido';
-
-export default function Home() {
+export default function Home () {
   return (
     <div className='bg-black w-full h-full'>
-      <div className='bg-black relative w-full h-[80vh] flex items-center justify-center'>
+      <div className='bg-black relative w-full h-[100vh] flex items-center justify-center'>
         <Image
-          src='/assets/image1.webp'
+          src='/assets/img2.webp'
           alt='TheCousins'
           layout='fill'
           objectFit='cover'
-          className='opacity-70'
+          className='opacity-70 blur-xs '
         />
-        <div className='absolute text-center text-white'>
-          <TextGenerateEffect words={text} className='text-4xl font-bold font-poppins' />
-          <TextGenerateEffect words={text2} className='text-2xl font-poppins' />
+
+        <div className='absolute text-center'>
+          <TextGenerateEffect
+            words={text}
+            className='text-7xl font-poppins text-white '
+          />
+          <TextGenerateEffect
+            words={text2}
+            className='text-2xl font-poppins text-white'
+          />
         </div>
       </div>
-      <div className='relative w-full h-[80vh] '>
-        <h1 className='text-orange-500 text-4xl font-bold text-center font-openSans'>Holaaa gaspar</h1>
-      </div>
+      <motion.div
+        className='relative w-full h-[80vh]'
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+      >
+        <div className='absolute text-center'>
+          <TextGenerateEffect
+            words={text3}
+            className='text-5xl font-poppins text-white '
+          />
+        </div>
+      </motion.div>
     </div>
-  );
+  )
 }
