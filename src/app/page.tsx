c93f5cf984129e0f25a070b1419a7fc9b1eb5c2e
heyitsGaspar <israel.chay.32@gmail.com>
2025-05-14 12:57:56 -0600

    add new section and component card-stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import { LampContainer } from '@/components/ui/lamp'
 
 const text = 'Bienvenidos a Los Primos'
 const text2 = 'Hamburguesas, hot dogs, esquites, y más...'
+const text3 = 'Explora nuestro Menú'
+const text4 = 'Desde hamburguesas jugosas, esquites con el toque perfecto y postres irresistibles. En Los Primos tenemos antojos para todos los gustos.¡Descubre lo que tenemos para ti y déjate sorprender!'
 // const text3 = 'Explora nuestros productos '
 
 export default function Home () {
@@ -54,9 +56,7 @@ export default function Home () {
               Explora <br />
               nuestros productos
             </motion.h1>
-           
           </LampContainer>
-
         </div>
       </motion.div>
       <motion.div
@@ -70,7 +70,38 @@ export default function Home () {
           <InfiniteMovingCardsFoods />
         </div>
       </motion.div>
-      
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+        viewport={{ once: true }}
+        className='bg-black relative w-full h-[100vh] flex items-start p-8'
+      >
+        <Image
+          src='/assets/img7.webp'
+          alt='TheCousins'
+          layout='fill'
+          objectFit='cover'
+          className='opacity-70 blur-xs '
+        />
+
+        <div className='relative z-10 flex flex-col md:flex-row gap-8 w-full max-w-6xl'>
+          <div className='md:w-1/2 text-white flex flex-col items-center justify-center'>
+            <TextGenerateEffect
+              words={text3}
+              className='text-4xl font-poppins text-white '
+            />
+            <TextGenerateEffect
+              words={text4}
+              className='text-2xl pt-4 text-center font-poppins text-white'
+            />
+          </div>
+          <div className='bg-white/20 backdrop-blur-md  flex items-center justify-center rounded-lg p-4 text-white'>
+               <h3 className="text-lg font-semibold">Card 2</h3>
+        <p>Otro contenido</p>
+          </div>
+        </div>
+      </motion.div>
     </div>
   )
 }
